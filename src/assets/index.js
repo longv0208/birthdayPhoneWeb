73282dafa122fef9e011822a989cdab815c0fcfd
longnhhe186624 <hailongv2004@gmail.com>
2025-10-30 13:50:48 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import bg from './bg.jpg'
+import confetti from './confetti.gif'
+import CakeSVG from './svg/CakeSVG'
+
+// Import ảnh mới
+import img1 from './d98af3fd-35ea-4802-bd49-293fc065d819.jpg'
+import img2 from './f0b2f853-8499-4d1d-87e4-a1ae6de2f3ac.jpg'
+import img3 from './8a1337a6-c9fb-4e4e-961f-f1d051044114.jpg'
+import img4 from './472769147_1647151499556702_3378510080850676291_n.jpg'
+import img5 from './af46bcf2-ef57-4536-bdf6-f0e0da95119b.jpg'
+import img6 from './45f20a32-22a2-4319-939a-f25f39c071d4.jpg'
+
+export {
+    bg,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    confetti,
+    CakeSVG
+}
